911319b59fedf79a2e248f9a4320c67a1a2b250f
Miguel A <migueldude27@gmail.com>
2022-12-15 14:31:13 -0500

    js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+function generatePassword(){
+  //strings do get characters from 
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var num = "0123456789";
+  var specs = "!@#$%^&*()+-=_";
 
+  //boolean vars 
+  var lcheck, ucheck, scheck;
+  
+  //the prompts for the length of the password
+  let lenPass = prompt("How long do you want the password to be? (8-128)");
+  //prompts to include (or not) lowercase, uppercase, numeric, and/or special characters
+  //lower 
+  let lowCase = prompt("Do you want lowercase characters? (Y or N)" );
+  if ((lowCase == 'Y' || lowCase == 'y') ||(lowCase == 'N' || lowCase == 'n')){
+
+    if(lowCase == 'Y' || lowCase == 'y'){
+      lcheck = true;
+    }
+    else{
+      lcheck = false;
+    }
+
+  }
+  //upper
+  let upCase = prompt("Do you want uppercase characters? (Y or N)");
+  //numeric
+  let numCase = prompt("Do you want the password to be numeric? (Y or N)");
+  //special characeters
+  let speCase = prompt("Do you want special chatracters? (Y or N)");
+
+  for(let x = 0; x <= lenPass;x++){
+    
+    //
+
+
+  }
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");

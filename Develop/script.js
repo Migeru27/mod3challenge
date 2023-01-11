fddb2899a44a1ce276bbc969fc00a1e0b1fdd2f9
Miguel A <migueldude27@gmail.com>
2023-01-11 02:44:57 -0500

    added more progress with loops

--- Develop/script.js
+++ Develop/script.js
@@ -4,18 +4,20 @@ function generatePassword(){
   var alpha = "abcdefghijklmnopqrstuvwxyz";
   var num = "0123456789";
   var specs = "!@#$%^&*()+-=_";
+  var result = "";
 
   //boolean vars 
-  var lcheck, ucheck, scheck;
+  var lcheck, ucheck, ncheck, scheck;
   
   //the prompts for the length of the password
   let lenPass = prompt("How long do you want the password to be? (8-128)");
+  
   //prompts to include (or not) lowercase, uppercase, numeric, and/or special characters
   //lower 
   let lowCase = prompt("Do you want lowercase characters? (Y or N)" );
-  if ((lowCase == 'Y' || lowCase == 'y') ||(lowCase == 'N' || lowCase == 'n')){
+  if ((lowCase === 'Y' || lowCase === 'y') || (lowCase === 'N' || lowCase === 'n')){
 
-    if(lowCase == 'Y' || lowCase == 'y'){
+    if(lowCase === 'Y' || lowCase === 'y'){
       lcheck = true;
     }
     else{
@@ -25,10 +27,58 @@ function generatePassword(){
   }
   //upper
   let upCase = prompt("Do you want uppercase characters? (Y or N)");
+  if((upCase === 'Y' || upCase === 'y') || (upCase === 'N' || upCase === 'n')){
+    
+    if(upCase === 'Y' || upCase === 'y'){
+      ucheck = true;
+    }
+    else{
+      ucheck = false;
+    }
+  }
   //numeric
   let numCase = prompt("Do you want the password to be numeric? (Y or N)");
+
+  if((numCase === 'Y' || numCase === 'y') || (numCase === 'N' || numCase === 'n')){
+    
+    if(numCase === 'Y' || numCase === 'y'){
+      ncheck = true;
+    }
+    else{
+      ncheck = false;
+    }
+  }
   //special characeters
   let speCase = prompt("Do you want special chatracters? (Y or N)");
+
+  if((speCase === 'Y' || speCase === 'y') || (speCase === 'N' || speCase === 'n')){
+    
+    if(speCase === 'Y' || speCase === 'y'){
+      ucheck = true;
+    }
+    else{
+      ucheck = false;
+    }
+  }
+
+  //loops for each result 
+
+if(lcheck === true ){
+  for(let x = 0; x <= lenPass;x++){
+    
+    switch(Math.floor(Math.random()*3)){
+      case 0:
+        // this get a random value from the alpha array and adds onto the final password
+        result += alpha[Math.floor(Math.random()*alpha.length)];
+        break;
+      case 1:
+        result  += 
+    }
+
+
+  }
+}
+
 
   for(let x = 0; x <= lenPass;x++){
     

04e5b6d05ceddc81a5f74bfd0124a7b191baa0f3
Miguel A <migueldude27@gmail.com>
2023-01-11 05:01:33 -0500

    added more progress with loops

--- Develop/script.js
+++ Develop/script.js
@@ -54,40 +54,107 @@ function generatePassword(){
   if((speCase === 'Y' || speCase === 'y') || (speCase === 'N' || speCase === 'n')){
     
     if(speCase === 'Y' || speCase === 'y'){
-      ucheck = true;
+      scheck = true;
     }
     else{
-      ucheck = false;
+      scheck = false;
     }
   }
 
-  //loops for each result 
+  //loops for each result
+  //first case if all is true
+  if((lcheck === true && ucheck === true) && (ncheck === true && scheck === true) ){
+    for(let x = 0; x <= lenPass-1;x++){
+      //switch case that will randomize which character is picked
+      switch(Math.floor(Math.random()*4)){
+        //lower
+        case 0:
+          result += alpha[Math.floor(Math.random()*alpha.length)];
+          break;
+        //upper
+        case 1:
+          result += alpha[Math.floor(Math.random()*alpha.length)].toUpperCase();
+          break;
+        //number
+        case 2:
+          result += num[Math.floor(Math.random()*num.length)];
+          break;
+        //special
+        case 3:
+          result += specs[Math.floor(Math.random()*specs.length)];
+          break;
+          
 
-if(lcheck === true ){
-  for(let x = 0; x <= lenPass;x++){
-    
-    switch(Math.floor(Math.random()*3)){
+      }
+    }
+  }
+  //num and special are true
+  else if((lcheck === false && ucheck === false) && (ncheck === true && scheck === true)){
+    switch(Math.floor(Math.random()*2)){
+      //number
       case 0:
-        // this get a random value from the alpha array and adds onto the final password
+        result += num[Math.floor(Math.random()*num.length)];
+        break;
+      //special
+      case 1:
+        result += specs[Math.floor(Math.random()*specs.length)];
+        break;
+    }
+  }
+  //lower and upper are true
+  else if((lcheck === true && ucheck === true) && (ncheck === false && scheck === false)){
+    switch(Math.floor(Math.random()*2)){
+      //lower
+      case 0:
+        result += alpha[Math.floor(Math.random()*num.length)];
+        break;
+      //upper
+      case 1:
+        result += alpha[Math.floor(Math.random()*specs.length)].toUpperCase();
+        break;
+    }
+  }
+  //lower, number, and special are true
+  else if((lcheck === true || ucheck === false) && (ncheck === true && scheck === true)){
+    switch(Math.floor(Math.random()*3)){
+      //lower
+      case 0:
         result += alpha[Math.floor(Math.random()*alpha.length)];
         break;
+      //number
       case 1:
-        result  += 
+        result += num[Math.floor(Math.random()*num.length)];
+        break;
+      //special
+      case 2:
+        result += specs[Math.floor(Math.random()*specs.length)];
+        break;
     }
-
-
   }
-}
-
-
-  for(let x = 0; x <= lenPass;x++){
-    
-    //
-
-
+  //lower, upper, and number are true 
+  else if((lcheck === true && ucheck === true) && (ncheck === true || scheck === false)){
+    switch(Math.floor(Math.random()*3)){
+      //lower
+      case 0:
+        result += alpha[Math.floor(Math.random()*alpha.length)];
+        break;
+      //upper
+      case 1:
+        result += alpha[Math.floor(Math.random()*alpha.length)].toUpperCase();
+        break;
+      //number
+      case 2:
+        result += num[Math.floor(Math.random()*num.length)];
+        break;
+    }
   }
 
 
+  
+
+
+  console.log(result);
+  return result;
 }
 
 // Get references to the #generate element
